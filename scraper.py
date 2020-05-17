3ea3bdaf51bd4ca74419305374f4824c70788470
sassone <mattia2002.ferrari@gmail.com>
2020-05-17 12:21:34 +0200

    cambiata la versione del driver, si è creata la cartella con la classe dello scraper

--- scraper.py
+++ scraper.py
@@ -1,4 +1,15 @@
+from selenium import webdriver
+
 class Scraper:
+    
     def __init__(self):
         self.result = None
+        self.driver = webdriver.Chrome("./driver/chromedriver.exe")
+        self.div = None
+    
+    def getContent(self):
+        self.driver.get("https://www.thisworddoesnotexist.com/")
+        self.result = self.driver.page_source
         
+    def getDiv(self):
+        self.div ="sos"

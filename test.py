6f820f9df1f184ac40bb0e28706687d618288caa
nilaerdna <andrea.lin3110@gmail.com>
2020-05-17 23:16:20 +0200

    stampa qualcosa di vagamente accettabile

--- test.py
+++ test.py
@@ -1,6 +1,7 @@
 from PIL import Image, ImageFont, ImageDraw
 from scraper import Scraper
 import textwrap
+import time
 
 # 0: background color; 1: text color; 2: type and example color
 colors = [(8, 8, 45, 255), (255, 255, 255, 255), (255, 255, 255, 128)]
@@ -35,10 +36,9 @@ def drawMultipleLineText(text, fontFile, fontSize, sassone, color, iterator):
 
     for index, line in enumerate(lines):
         lineWidth, lineHeight = font.getsize(line)
-
-        print(font.getsize(line))
-        draw.multiline_text(((imgWidth - lineWidth) / 2, heightList[sassone] * iterator), line, font=font, fill=color)
+        draw.multiline_text(((imgWidth - lineWidth) / 2, (imgHeight - lineHeight) / 4 + heightList[sassone] * iterator), line, font=font, fill=color)
         sassone += 1
+        img.show()
 
 # Lists
 # Type, Title, Syllables, Definition, Example
@@ -55,21 +55,21 @@ def textHeight(text, fontFile, fontSize, color, iterator):
     for line in lines:
         lineWidth, lineHeight = font.getsize(line)
         heightList.append(lineHeight)
+    return sum(heightList)
 
 def main():
     scraper.getContent()
     sassone = 0
+    
     for i, textString in enumerate(scraper.values):
         # print(i, textString)
-        textHeight(textString, fontsList[i], dimensionsList[i]*sizeMultiplier, colorsList[i], i+1)
+        heightSum = textHeight(textString, fontsList[i], dimensionsList[i]*sizeMultiplier, colorsList[i], i+1)
 
     for i, textString in enumerate(scraper.values):
         drawMultipleLineText(textString, fontsList[i], dimensionsList[i]*sizeMultiplier, sassone, colorsList[i], i+1)
 
-    print(heightList)
-
-    img.save("test.png")
-    img.show()
+    # img.save("test.png")
+    # img.show()
 
 if __name__ == "__main__":
     main()

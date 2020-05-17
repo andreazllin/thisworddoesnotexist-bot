87ea31df2bce6dc4ddadeaf6e9870a3af63a8537
nilaerdna <andrea.lin3110@gmail.com>
2020-05-17 21:11:18 +0200

    calcolo altezza totale

--- scraper.py
+++ scraper.py
@@ -1,28 +1,37 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.chrome.options import Options
 from bs4 import BeautifulSoup
+
+
+chrome_options = Options()
+chrome_options.add_argument("--headless")
 
 class Scraper:
     
     def __init__(self):
         self.result = None
-        self.driver = webdriver.Chrome("./driver/chromedriver.exe")
+        self.driver = webdriver.Chrome("./driver/chromedriver.exe", chrome_options=chrome_options)
         self.data = {}
-    
+        self.values = []
+
     def stringClean(self, x):
         x = x.replace("\n", "")
         x = x.strip()
         return x
     
     def getContent(self):
-        self.driver.get("https://www.thisworddoesnotexist.com/")
+        #self.driver.get("https://www.thisworddoesnotexist.com/")
+        self.driver.get("https://l.thisworddoesnotexist.com/KNTV")
         self.result = self.driver.page_source
         self.driver.quit()
         self.getDiv()
         
     def getDiv(self):
         soup = BeautifulSoup(self.result, features="html.parser")
-        self.data['word'] = soup.find('div', attrs={'id':'definition-word'}).text
         self.data['type'] = self.stringClean(soup.find('div', attrs={'id':'definition-pos'}).text)
+        self.data['word'] = self.stringClean(soup.find('div', attrs={'id': 'definition-word'}).text)
         self.data['syllables'] = self.stringClean(soup.find('div', attrs={'id':'definition-syllables'}).text)
-        self.data['definition'] = soup.find('div', attrs={'id':'definition-definition'}).text
-        self.data['example'] = soup.find('div', attrs={'id': 'definition-example'}).text
+        self.data['definition'] = self.stringClean(soup.find('div', attrs={'id':'definition-definition'}).text)
+        self.data['example'] = self.stringClean(soup.find('div', attrs={'id': 'definition-example'}).text)
+        self.values = self.data.values()
--- test.py
+++ test.py
@@ -1,6 +1,6 @@
 from PIL import Image, ImageFont, ImageDraw
-from scraper import Scraper as Scrap
-from datetime import datetime
+from scraper import Scraper
+import textwrap
 
 # 0: background color; 1: text color; 2: type and example color
 colors = [(8, 8, 45, 255), (255, 255, 255, 255), (255, 255, 255, 128)]
@@ -12,31 +12,63 @@ fontHeavy = "font/Heavy.ttf"
 fontRegular = "font/Regular.ttf"
 fontRegularItalic = "font/RegularItalic.ttf"
 
-sizeMultiplier = 2
-
 # Scraped Data
-scraper = Scrap()
+scraper = Scraper()
 
-def drawText(text, coords, fontFile, fontSize=10, color=(255, 255, 255, 128)):
+"""
+def drawText(text, coords, fontFile, fontSize, color=(255, 255, 255, 128)):
     font = ImageFont.truetype(fontFile, fontSize)
     draw = ImageDraw.Draw(img)
-    draw.multiline_text(coords, text, font=font, fill=color, align="left")
+    draw.text(coords, text, font=font, fill=color, align="left")
+"""
 
+
+def drawMultipleLineText(text, fontFile, fontSize, color=(255, 255, 255, 128), iterator=1):
+    font = ImageFont.truetype(fontFile, fontSize)
+    draw = ImageDraw.Draw(img)
+
+    # Image Size
+    imgWidth, imgHeight = img.size
+
+    textY = 50 # Y position where it starts
+
+    lines = textwrap.wrap(text, width=60)
+
+    for line in lines:
+        lineWidth, lineHeight = font.getsize(line)
+
+        print(font.getsize(line))
+        draw.multiline_text(((imgWidth - lineWidth) / 2, 100 + (50*iterator)), line, font=font, fill=color)
+        lastY = lineHeight
+
+# Lists
+# Type, Title, Syllables, Definition, Example
+dimensionsList = [13, 64, 16, 16, 16]
+fontsList = [fontRegular, fontHeavy, fontHeavy, fontRegular, fontRegularItalic]
+colorsList = [colors[2], colors[1], colors[1], colors[1], colors[2]]
+sizeMultiplier = 2
+heightList = []
+
+
+def textHeight(text, fontFile, fontSize, color=(255, 255, 255, 128), iterator=1):
+    font = ImageFont.truetype(fontFile, fontSize)
+    lines = textwrap.wrap(text, width=60)
+    for line in lines:
+        lineWidth, lineHeight = font.getsize(line)
+        heightList.append(lineHeight)
+
+def main():
+    scraper.getContent()
+
+    for i, textString in enumerate(scraper.values):
+        # print(i, textString)
+        textHeight(textString, fontsList[i], dimensionsList[i]*sizeMultiplier, color=colorsList[i], iterator=i+1)
+        # drawMultipleLineText(textString, fontsList[i], dimensionsList[i]*sizeMultiplier, color=colorsList[i], iterator=i+1)
+
+    print(heightList)
+
+    #img.save("test.png")
+    #img.show()
 
 if __name__ == "__main__":
-    scraper.getContent()
-    print(scraper.data)
-    
-    # Type
-    drawText(scraper.data['type'], (30, 10), fontRegular, fontSize=13*sizeMultiplier, color=colors[2])
-    # Title
-    drawText(scraper.data['word'], (30, 15), fontHeavy, fontSize=64*sizeMultiplier, color=colors[1])
-    # Syllables
-    drawText(scraper.data['syllables'], (30, 150), fontHeavy, fontSize=16*sizeMultiplier, color=colors[1])
-    # Definition
-    drawText(scraper.data['definition'], (30, 500), fontRegular, fontSize=16*sizeMultiplier, color=colors[1])
-    # Example
-    drawText(scraper.data['example'], (30, 650), fontRegularItalic, fontSize=16*sizeMultiplier, color=colors[2])
-    
-    img.save("test.png")
-    img.show()
+    main()

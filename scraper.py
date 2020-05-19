fbcb7e2ebd7b36ce230b267d690ae90c608b42dd
nilaerdna <andrea.lin3110@gmail.com>
2020-05-19 18:48:22 +0200

    Replace Selenium with Requests

--- scraper.py
+++ scraper.py
@@ -1,17 +1,12 @@
-from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.chrome.options import Options
+import requests
 from bs4 import BeautifulSoup
 
-
-chrome_options = Options()
-chrome_options.add_argument("--headless")
+URL = 'https://www.thisworddoesnotexist.com/'
 
 class Scraper:
     
     def __init__(self):
         self.result = None
-        self.driver = webdriver.Chrome("./driver/chromedriver.exe", chrome_options=chrome_options)
         self.data = {}
         self.values = []
 
@@ -21,13 +16,8 @@ class Scraper:
         return x
     
     def getContent(self):
-        self.driver.get("https://www.thisworddoesnotexist.com/")
-        self.result = self.driver.page_source
-        self.driver.quit()
-        self.getDiv()
-        
-    def getDiv(self):
-        soup = BeautifulSoup(self.result, features="html.parser")
+        page = requests.get(URL)
+        soup = BeautifulSoup(page.content, features="html.parser")
         self.data['type'] = self.stringClean(soup.find('div', attrs={'id':'definition-pos'}).text)
         self.data['word'] = self.stringClean(soup.find('div', attrs={'id': 'definition-word'}).text)
         self.data['syllables'] = self.stringClean(soup.find('div', attrs={'id':'definition-syllables'}).text)
@@ -36,6 +26,5 @@ class Scraper:
         self.values = self.data.values()
         print(self.values)
         if (len(self.data['word']) > 15):
-            self.driver.refresh()
             print("Word longer than 15 characters!")
             self.getContent()
